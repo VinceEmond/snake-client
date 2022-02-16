b2fa8477dce1d21e3cec892ee6ab720f45fc275d
VinceEmond <vince.emond@icloud.com>
2022-02-16 16:45:37 +0000

    Initial Commit: Created play.js file

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: // IP address here,
+    port: // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+conn.on("connect", (data) => {
+  console.log("Data from server:",data);
+})
+
+console.log("Connecting ...");
+connect();
