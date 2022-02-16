7e3e7d6247e154c870169eb160e8a35db90bc79a
VinceEmond <vince.emond@icloud.com>
2022-02-16 23:28:35 +0000

    Updated things

--- client.js
+++ client.js
@@ -2,16 +2,16 @@ const { builtinModules } = require('module');
 const net = require('net');
 
 const conn = net.createConnection({
-  host: "localhost",
-  port: 3000
+  host: "165.227.47.243",
+  port: 50541
 });
 
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: 3000// PORT number here,
+    host: "165.227.47.243",
+    port: 50541
   });
 
   // interpret incoming data as text
