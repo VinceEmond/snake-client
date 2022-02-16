8002ab3dad178e74d4f4a3379c6d24d5fc8ad56f
VinceEmond <vince.emond@icloud.com>
2022-02-16 23:50:09 +0000

    Added name

--- client.js
+++ client.js
@@ -1,14 +1,16 @@
 const { builtinModules } = require('module');
 const net = require('net');
 
-const conn = net.createConnection({
-  host: "165.227.47.243",
-  port: 50541
-});
+// const conn = net.createConnection({
+//   host: "165.227.47.243",
+//   port: 50541
+// });
 
 
 // establishes a connection with the game server
 const connect = function() {
+
+
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
@@ -17,13 +19,18 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  //As soon as your connected, do this
+  conn.on("connect", (data) => {
+    console.log("Data from server:", data);
+    console.log("Succesfully connected to server");
+    conn.write(`Name: 8=>`);
+  });
+
   return conn;
 };
 
 
-conn.on("connect", (data) => {
-  console.log("Data from server:", data);
-});
+
 
 
 module.exports = {
