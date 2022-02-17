74040d519b7035894340a14528749606f7032bf6
VinceEmond <vince.emond@icloud.com>
2022-02-17 00:10:30 +0000

    Played with move up

--- client.js
+++ client.js
@@ -7,15 +7,17 @@ const net = require('net');
 // });
 
 
+const conn = net.createConnection({
+  host: "165.227.47.243",
+  port: 50541
+});
+
+
 // establishes a connection with the game server
 const connect = function() {
 
 
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-
+  
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
@@ -24,10 +26,52 @@ const connect = function() {
     console.log("Data from server:", data);
     console.log("Succesfully connected to server");
     conn.write(`Name: 8=>`);
+
   });
+
+ 
+
+
+
+  // "Move: up";
+  // "Move: down";
+  // "Move: left";
+  // "Move: right";
+
+  // conn.on('data', function(key) {
+
+  //   // ctrl-c to be able to exit
+  //   if (key === '\u0003') {
+  //     console.log("Thanks for using me, ciao!");
+  //     process.exit();
+  //   }
+  
+  //   if (inputs[key]) {
+  //     if (inputs[key] === 1) {
+  //       console.log("BEEP BEEP MOTHERFUCKER!");
+  //       process.stdout.write('\x07');
+  //     } else if (inputs[key] !== 1) {
+  //       createAlarm(inputs[key]);
+  //     }
+  //   }
+  // });
+
 
   return conn;
 };
+
+// conn.on("connect", (data) => {
+//   setTimeout(() => {
+//     conn.write(`Move: up`);
+//   }, 1000);
+// });
+
+// conn.on("connect", (data) => {
+//   setTimeout(() => {
+//     conn.write(`Move: up`);
+//   }, 2000);
+// });
+
 
 
 
